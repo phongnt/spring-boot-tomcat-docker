0ba62e7846f2c756ebe80042465f55a87e789cfb
phongnt <genycopedison@gmail.com>
2022-04-04 10:56:41 +0700

    Initial commit

---
+++ src/test/java/com/github/phongnt/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.phongnt.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
