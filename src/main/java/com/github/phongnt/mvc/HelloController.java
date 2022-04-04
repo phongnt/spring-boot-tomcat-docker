746ad7529ee03698a226ca19a8288266c68a1136
phongnt <genycopedison@gmail.com>
2022-04-04 13:44:11 +0700

    Adding Thymeleaf

---
+++ src/main/java/com/github/phongnt/mvc/HelloController.java
@@ -0,0 +1,17 @@
+package com.github.phongnt.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String handler(Model model, @RequestParam(value = "name") String name) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
