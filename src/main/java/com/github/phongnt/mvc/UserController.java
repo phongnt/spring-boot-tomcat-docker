514db4c25457e8f97d2cc62c176fcc44c5aedbda
phongnt <genycopedison@gmail.com>
2022-04-28 14:49:08 +0700

    Adding page to create new user

---
+++ src/main/java/com/github/phongnt/mvc/UserController.java
@@ -0,0 +1,14 @@
+package com.github.phongnt.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/users/registration")
+    public String showRegistrationForm() {
+        return "signup";
+    }
+
+}
